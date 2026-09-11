3dcfa42d22c3ba6d8c5b4a07ee015523e1512e46
Abhijit swain <swain.abhijit0119@gmail.com>
2026-09-11 09:15:48 +0530

    done with the chatbot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Projects from "@/components/sections/Projects";
 import Experience from "@/components/sections/Experience";
 import Education from "@/components/sections/Education";
 import Contact from "@/components/sections/Contact";
+import Chatbot from "@/components/ui/Chatbot";
 
 export default function Home() {
   return (
@@ -22,6 +23,7 @@ export default function Home() {
         <Contact />
       </main>
       <Footer />
+      <Chatbot />
     </>
   );
 }
